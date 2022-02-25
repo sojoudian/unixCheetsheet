68dc15acab3d85bf320ccca9898228bf69c1d270
sojoudian <sojodyan@gmail.com>
2022-02-25 00:11:17 -0500

    get all videos duration and add those durations

---
+++ getAllVideosInDirectoryDuration.sh
@@ -0,0 +1,6 @@
+for f in *.mp4; do
+	t=$(mediainfo --Inform="Video;%Duration/String3%" $f)
+	echo $t
+done | tr : \ | dc -f - -e '60o0ddd[+r60*+r60d**+z1<a]dsaxp'
+
+awk '{print $2}' 1 | tr : \ | dc -f - -e '60o0ddd[+r60*+r60d**+z1<a]dsaxp'
